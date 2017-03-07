526ea9189c1b5093d982e30c537b15439e8816f7
JoaoBraveCoding <joao.marcal12@hotmail.com>
2017-03-07 23:55:44 +0000

    Implemented savepassword in userlib Added some funcionality to the user application Added common classes proj to share classes between server and client-lib Fixed one bug in the database class of the server

--- client/src/main/java/pt/sec/client/ClientApplication.java
+++ client/src/main/java/pt/sec/client/ClientApplication.java
@@ -19,15 +19,32 @@ public class ClientApplication {
 
             while (true) {
 
-                System.out.print("Password Maneger initiated: ");
+                System.out.println("Password Manager initiated: ");
+                System.out.println("Chose one of the following: create_user, save_password, get_password");
                 String input = br.readLine();
-
+                
                 if ("Exit".equals(input)) {
                     System.out.println("CY@!");
                     System.exit(0);
                 }
-                
-                
+                else if("create_user".equals(input)) {
+                	
+                }
+                else if("save_password".equals(input)) {
+                	System.out.print("Domain: ");
+                    String domain = br.readLine();
+                    System.out.print("Username: ");
+                    String username = br.readLine();
+                    System.out.print("Password: ");
+                    String password = br.readLine();
+                    cl.save_password(domain, username, password);
+                }
+                else if("get_password".equals(input)) {
+                	
+                }
+                else {
+                    System.out.println("Try again! ^_^");
+                }
             }
 
         } catch (IOException e) {
--- client_lib/src/main/java/pt/sec/a03/client_lib/ClientLib.java
+++ client_lib/src/main/java/pt/sec/a03/client_lib/ClientLib.java
@@ -1,12 +1,26 @@
 package pt.sec.a03.client_lib;
 
 import java.security.KeyStore;
+import java.security.PublicKey;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.codec.binary.Base64;
+
+import pt.sec.a03.common_classes.CommonTriplet;
 
 
 public class ClientLib 
 {
 	private KeyStore ks;
-	
+	private Client client = ClientBuilder.newClient();
+	private WebTarget baseTarget = client.target("http://localhost:8080/PwServer/webapi/");
+	private WebTarget vaultTarget = baseTarget.path("vault");
+		
     public void init(KeyStore ks) {
     	this.ks = ks;
     }
@@ -18,7 +32,29 @@ public class ClientLib
     
     //Mar
     public void save_password(String domain, String username, String password) {
+    	CommonTriplet commonTriplet = new CommonTriplet(password, username, domain);
+    	PublicKey pubKey = null; // ks.getKey("PublicKey", "Pog123");
+    	String stringPubKey = Base64.encodeBase64String(pubKey.getEncoded());
+    	Response postResponse = vaultTarget
+    			.request()
+    			.header("public-key", stringPubKey)
+    			.post(Entity.json(commonTriplet));
     	
+		if (postResponse.getStatus() == 201) {
+			System.out.println("Sucess");
+		}
+		else if (postResponse.getStatus() == 400) {
+			System.out.println("Invalid argument");
+		}
+		else if (postResponse.getStatus() == 404) {
+			System.out.println("Data Not Found");
+		}
+		else if (postResponse.getStatus() == 500) {
+			System.out.println("Internal server error");
+		}
+		else {
+			System.out.println("Error");
+		}
     }
     
     //Tiago
